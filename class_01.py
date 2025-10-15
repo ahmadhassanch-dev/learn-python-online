b47fb6235afd99cd21a4f75d0aadc1e2d8dbcb99
ahmadhassanch-dev <ahmadhassanch.dev@gmail.com>
2025-10-15 22:12:18 +0500

    Python class 1 and 2  added!

---
+++ class_01.py
@@ -0,0 +1,9 @@
+# Variables 
+numbers = 456
+
+print(numbers)
+
+# ( ` ) is called Backtick and it is located below the escape key( esc)
+# ctrl + ` is used to open terminal in vs code
+
+
---
+++ class_02.py
@@ -0,0 +1,36 @@
+# Variable data types => str(string), int(integer), float(decimal), bool(boolean)
+
+# String
+name : str = 'my name is Hassan'
+name1 : str = "Hassan"
+name2 : str = '''my name is
+ Hassan'''
+name3 : str = """my name is 
+Hassan"""
+# print(name)
+# print(name1)
+# print(name2)
+# print(name3)
+
+
+# Integer
+age : int = 16
+year : int = 2024
+# print(age)
+# print(year)
+print(type(age))
+
+# Float
+price : float = 20.02
+# print(price)
+
+# Boolean
+
+is_true : bool = True
+is_false : bool = False
+# print(is_true)
+# print(is_false)
+
+
+
+
