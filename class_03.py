18b86ee002062d1de78279bc8d9b9d63400c255d
ahmadhassanch-dev <ahmadhassanch.dev@gmail.com>
2025-10-16 22:04:17 +0500

    Python class 3 Added!

---
+++ class_03.py
@@ -0,0 +1,96 @@
+# Arithmetic Operators
+# Base values
+num1 = 10
+num2 = 20
+# Addition (+)
+sum = num1 + num2
+# print(f"Sum {num1} + {num2} = {sum}")
+
+# Subtraction (-)
+subtract = num2 - num1
+# print(f"Subtract {num2} - {num1} = {subtract}")
+
+# Multiplication (*)
+multiply = num1 * num2
+# print(f"Multiply {num1} * {num2} = {multiply}")
+
+#  Division (/)
+n1 = 4
+n2 = 2
+
+divide = n1 / n2
+# print(f"Divide {n2} / {n1   } = {divide}")
+
+# Floor Division (//)
+floor_division = n2 // n1
+# print(f"Floor Division {n2} // {n1} = {floor_division}")
+
+# Modulus (%)
+modulus = n1 % n2
+# print(f"Modulus {n2} % {n1} = {modulus}")
+
+# Exponentiation (**)
+exponentiation = n1 ** n2
+# print(f"Exponentiation {n1} ** {n2} = {exponentiation}")
+
+
+# Input From User
+# num3 = int(input("Enter first Number: "))
+# num4 = int(input("Enter second Number: "))
+# sum1 = num3 + num4
+# print(f"Sum {num3} + {num4} = {sum1}")
+
+
+
+
+# Assignment
+# Question:
+# Write a Python program that calculates an employee’s 
+# monthly savings using only arithmetic operators.
+# The program should:
+
+
+# Take input for:
+
+
+# basic_salary
+
+# bonus
+
+# deductions (like tax, insurance, etc.)
+
+# expenses (like food, rent, bills, etc.)
+
+# Use arithmetic operations only to calculate:
+
+
+# Total salary (basic_salary + bonus - deductions)
+
+# Remaining savings after paying expenses
+
+# Display all results neatly.
+# Example Output:
+# Enter basic salary: 50000 
+# Enter bonus: 5000 
+# Enter deductions: 3000 
+# Enter expenses: 40000  
+# Total salary: 52000.0 
+# Remaining savings: 12000.0
+
+
+# basic_salary = float(input("Enter basic salary: "))
+# bonus = float(input("Enter bonus: "))
+# deductions = float(input("Enter deductions: "))
+# expenses = float(input("Enter expenses: "))
+
+# total_salary = basic_salary + bonus  - deductions
+# remaining_savings = total_salary - expenses
+
+# print(f"Total salary: {total_salary}")
+# print(f"Remaining savings: {remaining_savings}")
+
+# BODMAS Rule
+# DMAS rule
+# BODMAS stands for Brackets, Orders (i.e. powers and square roots, etc.), Division and Multiplication, Addition and Subtraction.
+tricky = 20 - 10 * 2 / 3
+print(tricky) 
