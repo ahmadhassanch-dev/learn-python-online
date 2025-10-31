e0a6d537c456a83d01905c29881cc7ddeb05349b
ahmadhassanch-dev <ahmadhassanch.dev@gmail.com>
2025-10-31 22:21:51 +0500

    Class 11 Added!

--- class_10.py
+++ class_10.py
@@ -45,6 +45,7 @@
 # number += 1
 # number += 1
 # number += 1
+# number += 1
 # number -= 1
 # number -= 1
 
---
+++ class_11.py
@@ -0,0 +1,24 @@
+# Error handling in Python
+
+# number = 10
+# print(number / 0)
+
+# number1 = int(input("Enter any number:"))
+# print(number1)
+
+# try - except - else and finally blocks
+try:
+    number1 = int(input("Enter any number:"))
+    print("This is from try block")
+except:
+    print("This is from except block")
+else:
+    print("This is from else block")
+finally:
+    print("End of the program")
+
+# Escape sequences in Python
+# print("Program \t continues...")
+# print("abd \ndef")
+
+# print("*\n**\n***\n****")
